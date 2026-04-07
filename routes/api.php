e517c5802dc0f52150dbf23a60b6319cb43947d8
Nola Ristiyanti <nolaristiyanti.com>
2026-04-08 00:15:42 +0700

    fix routes get products by id

--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,22 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\Api\AuthController;
 
 Route::apiResource('/categories', ProductCategoryController::class);
-Route::apiResource('/products', ProductController::class);
+// Route::apiResource('/products', ProductController::class);
 
 //WAJIB pakai token
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
 
 //Public Routes (tanpa auth)
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
