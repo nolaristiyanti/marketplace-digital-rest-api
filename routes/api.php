d94412cc9d593b5655133725fa350ca2a9212d33
Nola Ristiyanti <nolaristiyanti.com>
2026-04-11 22:24:41 +0700

    update endpoint code

--- routes/api.php
+++ routes/api.php
@@ -6,37 +6,33 @@ use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\Api\AuthController;
 
-// Route::apiResource('/categories', ProductCategoryController::class);
-// Route::apiResource('/products', ProductController::class);
+// WAJIB pakai token dan only admin can manage categories
+Route::middleware(['auth:sanctum', 'ensure.admin'])->group(function () {
+    Route::post('/categories', [ProductCategoryController::class, 'store']);
+    Route::put('/categories/{category}', [ProductCategoryController::class, 'update']);
+    Route::delete('/categories/{category}', [ProductCategoryController::class, 'destroy']);
+});
 
-//WAJIB pakai token
-Route::middleware('auth:sanctum')->group(function(){
-    Route::prefix('auth')->group(function () {
+// WAJIB pakai token dan only can be accessed by role : seller
+Route::middleware(['auth:sanctum', 'ensure.seller'])->group(function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{product}', [ProductController::class, 'update'])->middleware('ensure.owner');
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->middleware('ensure.owner');
+});
+
+Route::prefix('auth')->group(function () {
+    //Public Routes
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    //WAJIB pakai token
+    Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/me', [AuthController::class, 'me']);
-    });
-
-    // only can be accessed by role : seller
-    Route::middleware('ensure.seller')->group(function () {
-        Route::post('/products', [ProductController::class, 'store']);
-        Route::put('/products/{product}', [ProductController::class, 'update'])->middleware('ensure.owner');
-        Route::delete('/products/{product}', [ProductController::class, 'destroy'])->middleware('ensure.owner');
-    });
-
-    // only can be accessed by role : seller
-    Route::middleware('ensure.admin')->group(function () {
-        Route::post('/categories', [ProductCategoryController::class, 'store']);
-        Route::put('/categories/{category}', [ProductCategoryController::class, 'update']);
-        Route::delete('/categories/{category}', [ProductCategoryController::class, 'destroy']);
     });
 });
 
 //Public Routes
-Route::prefix('auth')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-});
-
 Route::get('/categories', [ProductCategoryController::class, 'index']);
 Route::get('/categories/{category}', [ProductCategoryController::class, 'show']);
 
