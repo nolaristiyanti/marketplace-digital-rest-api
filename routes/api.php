2357d35b5aa91ecd5534244d28de0d8a79e29c7f
Nola Ristiyanti <nolaristiyanti.com>
2026-04-11 22:14:16 +0700

    update endpoint name related to auth

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,10 @@ use App\Http\Controllers\Api\AuthController;
 
 //WAJIB pakai token
 Route::middleware('auth:sanctum')->group(function(){
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me', [AuthController::class, 'me']);
+    Route::prefix('auth')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
+    });
 
     // only can be accessed by role : seller
     Route::middleware('ensure.seller')->group(function () {
@@ -21,25 +23,19 @@ Route::middleware('auth:sanctum')->group(function(){
         Route::delete('/products/{product}', [ProductController::class, 'destroy'])->middleware('ensure.owner');
     });
 
-    // Route::post('/products', [ProductController::class, 'store']);
-    // Route::put('/products/{id}', [ProductController::class, 'update']);
-    // Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-
     // only can be accessed by role : seller
     Route::middleware('ensure.admin')->group(function () {
         Route::post('/categories', [ProductCategoryController::class, 'store']);
         Route::put('/categories/{category}', [ProductCategoryController::class, 'update']);
         Route::delete('/categories/{category}', [ProductCategoryController::class, 'destroy']);
     });
-
-    // Route::post('/categories', [ProductCategoryController::class, 'store']);
-    // Route::put('/categories/{id}', [ProductCategoryController::class, 'update']);
-    // Route::delete('/categories/{id}', [ProductCategoryController::class, 'destroy']);
 });
 
-//Public Routes (tanpa auth)
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+//Public Routes
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 Route::get('/categories', [ProductCategoryController::class, 'index']);
 Route::get('/categories/{category}', [ProductCategoryController::class, 'show']);
