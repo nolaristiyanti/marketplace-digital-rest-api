8fbb9f3680a2477577463a72d8297b21c5361e92
Nola Ristiyanti <nolaristiyanti.com>
2026-04-17 10:13:01 +0700

    Form Request untuk store product, set product.thumbnail menjadi nullable

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\JsonResponse;
 use App\Traits\ApiResponse;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
+use App\Http\Requests\StoreProductRequest;
 
 class ProductController extends Controller
 {
@@ -125,48 +126,16 @@ class ProductController extends Controller
         return $this->successResponse($mappedProduct, 'Detail produk berhasil diambil');
     }
 
-    public function store(Request $request): JsonResponse
+    public function store(StoreProductRequest $request): JsonResponse
     {
-        // 1. validasi input
-        $validated = $request->validate([
-            // 'seller_id' => 'required|exists:users,id', // sudah pakai middleware -> EnsureSeller.php
-            'category_id' => 'required|exists:product_categories,id',
-            'title' => 'required|string|max:255',
-            'description' => 'required|string',
-            'price' => 'required|numeric|min:0',
-            'rating' => 'required|numeric|min:0|max:10',
-            'file_path' => 'required|string',
-            'thumbnail' => 'nullable|string',
-            'status' => 'in:active,inactive'
-        ], [
-            // 'seller_id.required' => 'Id seller wajib diisi', // sudah pakai middleware -> EnsureSeller.php
-            'category_id.required' => 'Id kategori produk wajib diisi',
-            'title.required' => 'Title produk wajib diisi',
-            'description.required' => 'Deskripsi produk wajib diisi',
-            'price.required' => 'Harga produk wajib diisi',
-            'rating.required' => 'Rating produk wajib diisi',
-            'file_path.required' => 'Path file produk wajib diisi',
-        ]);
-
-        // sudah pakai middleware -> EnsureSeller.php
-        // 2. Validasi user
-        // $user = User::findOrFail($validated['seller_id']);
-        // if ($user->role !== 'seller') {
-        //     //Forbidden -> tidak punya hak akses
-        //     return $this->errorResponse('Hanya seller yang boleh menambahkan produk', 403);
-        // }
-
-        // 3. simpan data ke database
-        // $category = Product::create($validated);
         $product = Product::create([
-            ...$validated,
+            ...$request->validated(),
             'seller_id' => $request->user()->id
         ]);
 
         Cache::flush(); // hapus semua cache products
 
-        // 4. return response JSON
-        return $this->successResponse($product, 'Produk berhasil ditambahkan');
+        return $this->successResponse($product, 'Produk berhasil ditambahkan', 201);
     }
 
     public function update(Request $request, Product $product): JsonResponse
---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; //sudah pakai middleware EnsureSeller, jadi authorization sudah ditangani di sana
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category_id' => 'required|exists:product_categories,id',
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric|min:0',
+            'rating' => 'required|numeric|min:0|max:10',
+            'file_path' => 'required|string',
+            'thumbnail' => 'nullable|string',
+            'status' => 'nullable|in:active,inactive'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            // category_id
+            'category_id.required' => 'Id kategori produk wajib diisi',
+            'category_id.exists' => 'Kategori tidak ditemukan',
+
+            // title
+            'title.required' => 'Title produk wajib diisi',
+            'title.max' => 'Title maksimal 255 karakter',
+
+            // description
+            'description.required' => 'Deskripsi produk wajib diisi',
+            'description.string' => 'Deskripsi harus berupa teks',
+
+            // price
+            'price.required' => 'Harga produk wajib diisi',
+            // 'price.numeric' => 'Harga harus berupa angka', -> untuk belajar : ini akan di handle di exception bootstrap/app.php
+            'price.min' => 'Harga tidak boleh kurang dari 0',
+
+            // rating
+            'rating.required' => 'Rating produk wajib diisi',
+            'rating.min' => 'Rating minimal 0',
+            'rating.max' => 'Rating maksimal 10',
+
+            // file_path
+            'file_path.required' => 'Path file produk wajib diisi',
+            'file_path.max' => 'Path file maksimal 255 karakter',
+
+            // thumbnail
+            'thumbnail.max' => 'Thumbnail maksimal 255 karakter',
+
+            // status
+            'status.in' => 'Status harus bernilai active atau inactive',
+        ];
+    }
+
+    // protected function failedValidation(Validator $validator)
+    // {
+    //     // default Laravel saat validasi gagal. custom format response error
+    //     throw new HttpResponseException(response()->json([
+    //         'status' => false,
+    //         'message' => 'Validasi gagal',
+    //         'errors' => $validator->errors(),
+    //     ], 400));
+    // }
+}
